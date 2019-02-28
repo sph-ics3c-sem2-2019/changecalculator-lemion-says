d1b776165a3ac1b75c4225ef625db770eea5b364
Reyon1 <47535587+Reyon1@users.noreply.github.com>
2019-02-28 13:20:05 -0500

    we did it

--- Main.py
+++ Main.py
@@ -14,3 +14,26 @@ change=change%50
 num20=change//20
 print( num20, "x $20")
 change=change%20
+#how many 10s
+num10=change//10
+print( num10, "x $10")
+#how many 5s
+num5=change//5
+print( num5, "x $5")
+#how many 2s
+num2=change//2
+print( num2, "x $2")
+#how many 1s
+num1=change//1
+print( num1, "x $1")
+#how many 0.25s
+num0_25=change//0.25
+print( num0_25, "x $0.25")
+#how many 0.10s
+num0_10=change//0.10
+print( num0_10, "x $0.10")
+#how many 10s
+num0_05=change//0.05
+print( num0_05, "x $0.05")
+#Only print if you have a non-zero value
+
